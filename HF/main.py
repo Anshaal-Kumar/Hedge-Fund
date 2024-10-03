5ebdb5f3012fdc5189da7237f4aa3cf6bce1e6be
Anshaal-Kumar <anshaalkumar@gmail.com>
2024-10-03 17:00:09 +0000

    data processing

---
+++ HF/main.py
@@ -0,0 +1,46 @@
+# numpy
+# pandas
+# scikit
+# matplotlib
+# openpyxl
+# tensorflow
+
+
+import pandas as pd
+import numpy as np 
+import yfinance as yf 
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_squared_error, r2_score
+
+# PUTTING DATA IN EXCEL
+file_path = "Hedge-Fund/archive/SP500.csv"
+df = pd.read_csv(file_path)
+
+print(df.head())
+
+
+# DATA PROCESSING 
+
+
+
+# SPLITTING DATA 
+
+
+# ALGO MODEL SELECTION
+
+
+# EVALUATE MODEL
+
+
+# FINE TUNING 
+
+
+# PREDICT
+
+
+# VISUALIZE
+
+
+# DEPLOY
